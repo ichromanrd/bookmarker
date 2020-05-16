831fcac8b08c7123ceacac5eaaa2d6f312cafe6f
ichroman <ichromanrd@linecorp.com>
2020-05-16 12:11:12 +0700

    restart again

--- bookmarker.js
+++ bookmarker.js
@@ -1,10 +1,1 @@
-chrome.tabs.getCurrent(function(tab) {
-  //return tab.url;
-  alert('active url is: ' + tab.url);
-});
-
-// chrome.bookmarks.create({
-//   'parentId': null,
-//   'title': 'Ext bookmark doc',
-//   'url': url
-// });
+//  alert("etst doang nih");
